aad637b71762de165a7910647f1be9068e7a7cdc
Kamy <kamy.leeret@gmail.com>
2020-06-29 22:38:56 -0600

    Revert "removed old api file."
    This reverts commit a53112fdcbbf5a7e9e3879f5dd0e8d364982967b.

---
+++ client/src/utils/API.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+export default {
+  getMovements: function () {
+    return axios.get("/api/movements");
+  },
+  getWorkouts: function() {
+    return axios.get("/api/workouts");
+  },
+  getWorkouts: function(id) {
+    return axios.get("/api/workouts/" + id);
+  },
+  
+  deleteWorkouts: function(id) {
+    return axios.delete("/api/workouts/" + id);
+  },
+
+  saveWorkouts: function(workoutData) {
+    let name = workoutData.createdBy
+    return axios.post("/api/workouts/" + name, workoutData);
+  },
+  getUsers: function() {
+    return axios.get("/api/users")
+  },
+  getUser: function(userName) {
+    return axios.get("/api/users/"+userName);
+  },
+  checkPassword: function(userData) {
+    return axios.get("/api/users/" + userData.userName+"/"+userData.password);
+  },
+  createUser: function(userData) {
+    return axios.post("/api/users",userData)
+  }
+  
+};
