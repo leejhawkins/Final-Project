7d793425cd17cb4a66c8c585894d43b491fd5401
Kamy <kamy.leeret@gmail.com>
2020-06-30 07:21:24 -0600

    fixed parser error app is working again. (kamy)

--- client/src/pages/Users.js
+++ client/src/pages/Users.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import DeleteBtn from "../components/DeleteBtn";
+import DeleteBtn from "../components/Buttons/DeleteBtn";
 import Jumbotron from "../components/Jumbotron";
-import API from "../Utils/API";
+import API from "../utils/API";
 import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
 import { List, ListItem } from "../components/List";
