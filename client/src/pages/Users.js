145dd7c206a6faaf51d1ea8b64fe9e312a681095
leejhawkins <59981819+leejhawkins@users.noreply.github.com>
2020-07-01 10:32:24 -0600

    Update Users.js

--- client/src/pages/Users.js
+++ client/src/pages/Users.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Jumbotron from "../components/Jumbotron";
-import API from "../Utils/API";
+import API from "../utils/API";
 import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
 import { List, ListItem } from "../components/List";
